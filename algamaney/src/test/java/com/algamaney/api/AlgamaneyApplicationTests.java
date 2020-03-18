83df97f9a88be3df9f6036418d37f3b8ed4afb00
Thieles <thielesmartinsdosreis@gmail.com>
2020-03-18 00:33:56 -0300

    subindo projeto algaworks full stack

---
+++ algamaney/src/test/java/com/algamaney/api/AlgamaneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algamaney.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamaneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
